0026d262dfaad17e80f2e2d4284ee45d3d8da26e
Rey <igorpedro.machado@gmail.com>
2024-06-23 22:14:03 +0100

    Pagina Destination terminada

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -4,9 +4,7 @@ export default function Destination() {
 
     return (
         <main id="Destination">
-            <video controls autoPlay loop className="dashboard">
-                <source src="/Media/mixkit-fuel-meter-on-a-car-dashboard-20858-hd-ready.mp4" type="video/mp4"/>
-            </video>
+            <img src="/Media/Dashboard.png" alt="" className="dashboard"/>
             <section className="section2">
                 <h1> Sobre nós</h1>
                 <p>Além do comércio de gás, a DonLau&Cia também opera um serviço de táxi privado, proporcionando transporte seguro, confortável e pontual. Ideal tanto para deslocações diárias quanto para viagens turísticas, a empresa oferece uma experiência de viagem personalizada. Os motoristas são profissionais experientes, prontos para proporcionar uma viagem agradável, seja para negócios ou lazer.</p>
@@ -16,25 +14,31 @@ export default function Destination() {
                     <img src="/Media/404-4042710_circle-profile-picture-png-transparent-png.png" alt="" className="profilePicture1"/>
                     <h2>Paul Anderson</h2>
                     <img src="/Media/vecteezy_four-stars-rating-png-with-transparent-background_16016805.png" alt="" className='stars'/>
-                    <p>The service was amazing.</p>
+                    <p>The service was amazing</p>
                 </div>
                 <div className="review2">
                     <img src="/Media/404-4042710_circle-profile-picture-png-transparent-png.png" alt="" className="profilePicture1"/>
-                    <h2>Pedro Sanchez</h2>
+                    <h2>Maria Sanchez</h2>
                     <img src="/Media/vecteezy_four-stars-rating-png-with-transparent-background_16016805.png" alt="" className='stars'/>
-                    <p>Muy bueno, el conductor era agradable.</p>
+                    <p>Muy bueno, el conductor era agradable</p>
                 </div>
                 <div className="review3">
                     <img src="/Media/404-4042710_circle-profile-picture-png-transparent-png.png" alt=""className="profilePicture2"/>
                     <h2>Lucas Fernando</h2>
                     <img src="/Media/vecteezy_four-stars-rating-png-with-transparent-background_16016805.png" alt="" className='stars'/>
-                    <p>Adoreiii!! Super recomendo.</p>
+                    <p>Adoreiii!! Super recomendo</p>
                 </div>
                 <div className="review4">
                     <img src="/Media/404-4042710_circle-profile-picture-png-transparent-png.png" alt=""className="profilePicture3"/>
                     <h2>João Silva</h2>
                     <img src="/Media/vecteezy_four-stars-rating-png-with-transparent-background_16016805.png" alt="" className='stars'/>
-                    <p>Gostei muito serviço da DonLau Destination.</p>
+                    <p>Gostei muito do serviço da DonLau Destination</p>
+                </div>
+                <div className="review5">
+                    <img src="/Media/404-4042710_circle-profile-picture-png-transparent-png.png" alt=""className="profilePicture3"/>
+                    <h2>Carla Brandão</h2>
+                    <img src="/Media/vecteezy_four-stars-rating-png-with-transparent-background_16016805.png" alt="" className='stars'/>
+                    <p>Uma maravilha!!!</p>
                 </div>
             </section>
             <section className="section3">
@@ -43,7 +47,7 @@ export default function Destination() {
                     <div className="card-body">
                         <h2 className="card-title">Taxi Privado</h2>
                         <p className="card-description">Estou aqui para servi-lo</p>
-                        <p className="card-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Justo laoreet sit amet cursus sit amet dictum sit amet.</p>
+                        <p className="card-info">Precisa passear, ir ao medico, ir as compras ou viajar para uma outra cidade? Alugue um taxi privado para qualquer situação.</p>
                         <button className="card-btn">Book Driver </button>   
                     </div>
                 </div>
@@ -52,7 +56,7 @@ export default function Destination() {
                     <div className="card-body">
                         <h2 className="card-title">Sintra</h2>
                         <p className="card-description">Deixa-nos guiar-te</p>
-                        <p className="card-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Justo laoreet sit amet cursus sit amet dictum sit amet.</p>
+                        <p className="card-info">Vem conhecer o Palácio da Pena, o Castelo dos Mouros, a Quinta da Regalheira, o Palácio de Monserrate e muito mais.</p>
                         <button className="card-btn">Book Tour</button>   
                     </div>
                 </div>
